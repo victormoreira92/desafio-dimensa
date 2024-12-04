2f782a7a8e2b6e0c42d472cc51cf5d78c2d02364
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-12-04 15:51:34 -0300

    feat: Criação de ContentsController e adição de scopes

---
+++ app/controllers/api/v1/contents_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class ContentsController < ApplicationController
+      has_scope :by_genre
+      has_scope :by_year_range
+      has_scope :by_cast
+      has_scope :by_country
+      has_scope :by_type_content
+      has_scope :by_title
+
+      def index
+        @contents = apply_scopes(Content).all
+        render json: @contents
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :contents, only: [:index]
       post '/content_files/process_csv', to: 'content_files#process_csv'
     end
   end
-
+  root to: 'api/v1/contents#index'
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
